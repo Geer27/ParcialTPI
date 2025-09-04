1c2386cac96dd5415f4f4c0fb20e7a594f9ce4cb
Geer <gerrherrnandez721@gmail.com>
2025-09-04 12:53:15 -0600

    Cambio en la interfaz

--- conversor.php
+++ conversor.php
@@ -6,6 +6,23 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <form action="" method="post">
+        <label for="">Ingresa una temperatura</label>
+        <menu>
+            <option value="c_f">Convertir grados Celsius a Farenheit</option>
+            <option value="f_c">Convertir grados Farenheit a Celsius</option>
+            <button> Convertir </button>
+        </menu>
+    </form>
 </body>
 </html>
+
+<?php
+    if(_POST) {
+        try {
+            
+        }catch(e){
+            
+        }
+    }
+?> 
--- personajes.php
+++ personajes.php
@@ -6,11 +6,22 @@
     <title>Personajes</title>
 </head>
 <body>
+
+    <label for="">Imagen de Shrek</label>
     <img src="imagenes/shrek.jpg" alt="Imagen de Shrek" srcset="">
+    <label for="">Imagen de la Bartender</label>
     <img src="imagenes/bartender.jpg" alt="Imagen de bartender" srcset="">
+    <label for="">Imagen de Burro</label>
     <img src="imagenes/burro.jpg" alt="Imagen de burro" srcset="">
+    <label for="">Imagen de Galleta</label>
     <img src="imagenes/galleta.jpg" alt="Imagen de galleta" srcset="">
+    <label for="">Imagen de Lord Farquad</label>
     <img src="imagenes/lord.jpg" alt="Imagen de lord Farquad" srcset="">
+
+
+    
+
+
     
 </body>
 </html>
