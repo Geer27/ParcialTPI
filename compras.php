3849b6157b72718eb6b7816623bd0710e234029b
Geer <gerrherrnandez721@gmail.com>
2025-09-04 12:33:50 -0600

    Inicializacion de las imagenes y tabla

---
+++ compras.php
@@ -0,0 +1,27 @@
+<?php 
+
+    $compras = ["Producto A", 20.00, 15];
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <table>
+        <td>
+            <tr>Producto</tr>
+            <tr>Precio</tr>
+            <tr>Cantidad</tr>
+            <tr>Subtotal</tr>
+        </td>
+    </table>
+</body>
+</html>
+
+<?php 
+    
+?>
--- personajes.php
+++ personajes.php
@@ -6,6 +6,11 @@
     <title>Personajes</title>
 </head>
 <body>
+    <img src="imagenes/shrek.jpg" alt="Imagen de Shrek" srcset="">
+    <img src="imagenes/bartender.jpg" alt="Imagen de bartender" srcset="">
+    <img src="imagenes/burro.jpg" alt="Imagen de burro" srcset="">
+    <img src="imagenes/galleta.jpg" alt="Imagen de galleta" srcset="">
+    <img src="imagenes/lord.jpg" alt="Imagen de lord Farquad" srcset="">
     
 </body>
 </html>
